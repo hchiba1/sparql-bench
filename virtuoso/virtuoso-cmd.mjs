dc5041584b8930e0008397d68d09a18dfd4f726e
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2021-06-29 16:02:34 +0900

    Fix output for query time

--- virtuoso/virtuoso-cmd.mjs
+++ virtuoso/virtuoso-cmd.mjs
@@ -6,10 +6,10 @@ const fs = require('fs');
 let RED = '\x1b[31m';
 let GREEN = '\x1b[32m';
 let YELLOW = '\x1b[33m';
+let NC = '\x1b[0m'; // No Color
 
-function coloredLog(color, text) {
-  let NC = '\x1b[0m'; // No Color
-  console.log(`${color}${text}${NC}`);
+function coloredLog(color, text, dst=process.stderr) {
+  dst.write(`${color}${text}${NC}\n`);
 };
 
 
@@ -59,14 +59,21 @@ await $`SRC_DATA_DIR=${dir} docker-compose up -d db`;
 coloredLog(YELLOW, `Removind all existing triples...`);
 await tryUntilSucceed(`echo "DELETE FROM DB.DBA.RDF_QUAD;" | docker-compose exec -T db isql-v 1111 dba dba`, 100);
 coloredLog(YELLOW, `Loading data...`);
+$.verbose = true; // Temporary verbosity to show loading time
 await tryUntilSucceed(`echo "DB.DBA.TTLP_MT(file_to_string_output('/tmp/data/${srcName}'), '', 'http://example.com/example.ttl', 0);" | docker-compose exec -T db isql-v 1111 dba dba`, 100);
+console.log(''); // Newline
+$.verbose = commander.opts().verbose; // Set verbosity again
 
 if(queryPath) {
   await $`cp ${queryPath} ${spangDir}`;
   let queryName = path.basename(queryPath);
-  coloredLog(YELLOW, `Benchmarking by spang-bench...`);
+  coloredLog(YELLOW, `Executing spang...`);
   let result = await $`SPANG_DATA_DIR=${spangDir} docker-compose run spang spang2 --time -e http://db:8890/sparql /data/${queryName}`;
-  coloredLog(GREEN, result);
+  console.log(result.stdout);
+  let stderrLines = result.stderr.split("\n").filter(line => line.length > 0);
+  if(stderrLines) {
+    console.log(stderrLines[stderrLines.length - 1]); // Last line should be time of execution
+  }
 }
 
 if(commander.opts().down) {
